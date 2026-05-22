ea3d57056d2da9a38bfae0d4f4295799fa87d924
Becca <beccapowell.stuff@gmail.com>
2026-05-23 00:41:17 +1200

    CTX-028 Add memory summary session data

--- src/types/AiDraftBenchPluginData.ts
+++ src/types/AiDraftBenchPluginData.ts
@@ -1,12 +1,20 @@
 import { AiDraftBenchSettings } from "../config/defaultSettings";
 import { AiDraftBenchEntry } from "./AiDraftBenchEntry";
 
+export type AiDraftBenchMemorySummary = {
+	text: string;
+	updatedAt: string;
+	sourceEntryId?: string;
+	entryCount: number;
+};
+
 export type AiDraftBenchCurrentSessionData = {
 	id: string;
 	createdAt: string;
 	updatedAt: string;
 	entryCount: number;
 	userTitle?: string;
+	memorySummary?: AiDraftBenchMemorySummary;
 	entries: AiDraftBenchEntry[];
 };
 
